45d88ea089df853e1ac68c20610a6232e4ca27d1
Liam Fleming <fleming.squared@proton.me>
2024-06-17 21:41:13 +0100

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    return array.filter(function(elem) {
+        return !toRemove.includes(elem);
+    });
 };
 
 // Do not edit below this line
