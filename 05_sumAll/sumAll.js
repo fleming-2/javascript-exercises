fbf1515085c65a618814f3bdbdb90d7c97504ae5
Liam Fleming <fleming.squared@proton.me>
2024-06-18 01:40:24 +0100

    Solve exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(rangeStart, rangeEnd) {
+    if (rangeStart < 0 || rangeEnd < 0) {
+        return 'ERROR';
+    }
+    if (typeof rangeStart !== 'number' || typeof rangeEnd !== 'number') {
+        return 'ERROR';
+    }
 
+    // Enable working with ranges declared backwards
+    if (rangeStart > rangeEnd) {
+        let temp = rangeStart;
+        rangeStart = rangeEnd;
+        rangeEnd = temp;
+    }
+
+    let sum = 0;
+    for(let i = rangeStart; i <= rangeEnd; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
