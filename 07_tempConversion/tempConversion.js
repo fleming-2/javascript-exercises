add3512c309aa53128a872a581cae794ce71a6a1
Liam Fleming <fleming.squared@proton.me>
2024-06-18 02:28:16 +0100

    Solve exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  // Convert
+  degrees = (degrees - 32) * 5 / 9;
+
+  // Round to one decimal place
+  degrees = Math.round(degrees * 10) / 10;
+
+  return degrees;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  // Convert
+  degrees = (degrees * 9 / 5) + 32;
+
+  // Round to one decimal place
+  degrees = Math.round(degrees * 10) / 10;
+
+  return degrees;
 };
 
 // Do not edit below this line
