6bfba44b02ca8229d40144f3eb165c7c6e5ff83d
Liam Fleming <fleming.squared@proton.me>
2024-06-26 19:30:42 +0100

    Solve exercise 11_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+"use strict";
 
+const findTheOldest = function(people) {
+  let currentYear = new Date().getFullYear();
+
+  // Calculate ages for each person
+  let associatedAges = people.map(function(person) {
+    return {
+      person,
+      age: (person.yearOfDeath ?? currentYear) - person.yearOfBirth,
+    };
+  });
+
+  // Find oldest
+  let oldest = associatedAges.reduce(function(currentOldest, candidate) {
+    return (candidate.age > currentOldest.age) ? candidate : currentOldest;
+  });
+
+  return oldest.person;
 };
 
 // Do not edit below this line
