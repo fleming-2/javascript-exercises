444301912257b46d9485e66cd3fc8df92c8e0d37
Liam Fleming <fleming.squared@proton.me>
2024-06-26 17:48:30 +0100

    Solve exercise 09_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  n = parseInt(n);
+  if (n === 0) {
+    return 0;
+  }
+  if (!n || n < 0) {
+    return "OOPS";
+  }
 
+  let current = 1;
+  let previous = 0;
+  let temp;
+  for (let i = 1; i < n; i++) {
+    temp = current;
+    current += previous;
+    previous = temp;
+  }
+  return current;
 };
 
 // Do not edit below this line
